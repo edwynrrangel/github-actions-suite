5ce0173b233a5fd7b204f25222e40e3031c10b98
Edwyn Rangel <edwynrangel@gmail.com>
2024-05-11 00:33:43 -0500

    chore: Add YAML to Env action
    The commit adds a new action called "YAML to Env" that reads YAML files and sets environment variables. It includes a Dockerfile, an action.yaml file, and Go source code files. The action uses the gopkg.in/yaml.v3 package to parse the YAML file and convert it into environment variables. The resulting environment variables are written to the GITHUB_ENV file.

---
+++ actions/yaml-to-env/src/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	if os.Getenv("INPUT_YAML_FILE") == "" {
+		panic("no yaml file provided")
+	}
+	yamlFile, err := os.Open(os.Getenv("INPUT_YAML_FILE"))
+	if err != nil {
+		panic(err)
+	}
+	defer yamlFile.Close()
+
+	data, err := io.ReadAll(yamlFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var envs map[string]interface{}
+	err = yaml.Unmarshal(data, &envs)
+	if err != nil {
+		panic(err)
+	}
+
+	mapEnvs := mapEnvironmentVariables("", envs)
+
+	envFile := os.Getenv("GITHUB_ENV")
+	if envFile == "" {
+		panic("GITHUB_ENV not set")
+	}
+
+	file, err := os.OpenFile(envFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Write each environment variable to the file
+	for key, value := range mapEnvs {
+		_, err := file.WriteString(fmt.Sprintf("%s=%s\n", key, value))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func mapEnvironmentVariables(prefix string, envs interface{}) map[string]string {
+	mapEnvs := make(map[string]string)
+	switch envs := envs.(type) {
+	case map[string]interface{}:
+		for key, value := range envs {
+			fullKey := constructFullKey(prefix, key)
+			mapEnvironmentVariables(fullKey, value)
+		}
+	case []interface{}:
+		for i, item := range envs {
+			indexedKey := fmt.Sprintf("%s.%d", prefix, i)
+			mapEnvironmentVariables(indexedKey, item)
+		}
+	case map[interface{}]interface{}:
+		normalizedMap := convertMap(envs)
+		mapEnvironmentVariables(prefix, normalizedMap)
+	default:
+		mapEnvs[prefix] = fmt.Sprintf("\"%v\"", envs)
+	}
+
+	return mapEnvs
+}
+
+func constructFullKey(prefix, key string) string {
+	if prefix == "" {
+		return key
+	}
+	return prefix + "." + key
+}
+
+func convertMap(orig map[interface{}]interface{}) map[string]interface{} {
+	normalized := make(map[string]interface{})
+	for k, v := range orig {
+		if keyStr, ok := k.(string); ok {
+			normalized[keyStr] = v
+		}
+	}
+	return normalized
+}
